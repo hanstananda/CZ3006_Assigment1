0f6bd004ed4aa5cac771bf57dfe6fe7af08990e7
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-02-11 22:31:42 +0800

    implement timer functions

--- src/SWP.java
+++ src/SWP.java
@@ -1,11 +1,23 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 public class SWP {
     public static final int MAX_SEQ = 7;
-    public static final int NR_BUFS = 4;
+    public static final int NR_BUFS = (MAX_SEQ + 1) / 2;
     private int oldest_frame = 0;
     private PEvent event = new PEvent();
-    private Packet[] out_buf = new Packet[4];
+    private Packet[] out_buf = new Packet[NR_BUFS];
     private SWE swe = null;
     private String sid = null;
+
+    // protocol related variables
+    private boolean no_nak = true;
+    // timer related variables and constants
+    private Timer[] timers = new Timer[NR_BUFS];
+    private Timer ack_timer = new Timer();
+    private static final int TIMEOUT_INTERVAL = 500;
+    private static final int ACK_TIMEOUT_INTERVAL = 300;
 
     public SWP(SWE var1, String var2) {
         this.swe = var1;
@@ -35,6 +47,7 @@ public class SWP {
 
     }
 
+
     public void protocol6() {
         this.init();
 
@@ -57,15 +70,36 @@ public class SWP {
     }
 
     private void start_ack_timer() {
+        stop_ack_timer();
+        ack_timer = new Timer();
+        ack_timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                swe.generate_acktimeout_event();
+            }
+        }, ACK_TIMEOUT_INTERVAL);
     }
 
     private void start_timer(int var1) {
+        stop_timer(var1);
+        timers[var1 % NR_BUFS] = new Timer();
+        timers[var1 % NR_BUFS].schedule(new ProtocolTimerTask(var1), TIMEOUT_INTERVAL);
     }
 
     private void stop_ack_timer() {
+        try {
+            ack_timer.cancel();
+        } catch (Exception e) {
+            System.out.println("Failed to stop the acknowledge timer");
+        }
     }
 
     private void stop_timer(int var1) {
+        try {
+            timers[var1 % NR_BUFS].cancel();
+        } catch (Exception e) {
+            System.out.println("Failed to stop timer");
+        }
     }
 
     private void to_network_layer(Packet var1) {
@@ -81,5 +115,21 @@ public class SWP {
     private void wait_for_event(PEvent var1) {
         this.swe.wait_for_event(var1);
         this.oldest_frame = var1.seq;
+    }
+
+    // Helper class for implementing timer
+    private class ProtocolTimerTask extends TimerTask {
+        public int seq; // add an attribute seq to record the sequence number
+
+        public ProtocolTimerTask(int seq) {
+            super();
+            this.seq = seq;
+        }
+
+        // generate a timeout event of the recorded sequence number when the task is executed
+        @Override
+        public void run() {
+            swe.generate_timeout_event(this.seq);
+        }
     }
 }
